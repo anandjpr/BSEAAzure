6a0a871ebc575d3cb792837ffc24e88f6cf48c99
Anand <anand.sharma@intel.com>
2017-06-01 16:25:52 -0700

    New changes

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+"use strict";
+var gulp = require("gulp");
+var del = require("del");
+var sourcemaps = require('gulp-sourcemaps');
+
+/**
+ * Remove build directory.
+ */
+gulp.task('clean', function (cb) {
+    return del(["build"], cb);
+});
+
+/**
+ * Copy all resources that are not TypeScript files into build directory.
+ */
+gulp.task("resources", ["server", "app", "assets"], function () {
+    console.log("Building resources...");
+});
+/* copy the app core files to the build folder */
+gulp.task("app", ['index'], function(){
+    return gulp.src(["src/app/**", "!src/app/**/*.ts"])
+        .pipe(gulp.dest("build/app"));
+});
+/* get the index file to the root of the build */
+gulp.task("index", function(){
+    return gulp.src(["src/index.html","src/main.js","src/systemjs-angular-loader.js","src/systemjs.config.extras.js","src/systemjs.config.js"])
+        .pipe(gulp.dest("build"));
+});
+/* copy node server to build folder */
+gulp.task("server", function () {
+    return gulp.src(["index.js", "package.json"], { cwd: "src/server/**" })
+        .pipe(gulp.dest("build"));
+});
+/* styles and other assets */
+gulp.task("assets", function(){
+    return gulp.src(["src/styles.css"])
+        .pipe(gulp.dest("build"));
+});
+/**
+ * Copy all required libraries into build directory.
+     <script src="node_modules/core-js/client/shim.min.js"></script>
+
+    <script src="node_modules/zone.js/dist/zone.js"></script>
+    <script src="node_modules/systemjs/dist/system.src.js"></script>
+
+    <script src="systemjs.config.js"></script>
+*/
+gulp.task("libs", function () {
+    return gulp.src([
+        'core-js/client/shim.min.js',
+        'zone.js/dist/zone.js',
+        'es6-shim/es6-shim.min.js',
+        'systemjs/dist/system-polyfills.js',
+        'angular2/bundles/angular2-polyfills.js',
+        'angular2/es6/dev/src/testing/shims_for_IE.js',
+        'systemjs/dist/system.src.js',
+        'rxjs/bundles/Rx.js',
+        'angular2/bundles/angular2.dev.js',
+        'angular2/bundles/router.dev.js'
+    ], { cwd: "node_modules/**" }) /* Glob required here. */
+        .pipe(gulp.dest("build/node_modules"));
+});
+/**
+ * Build the project.
+ */
+gulp.task("default", ['resources', 'libs'], function () {
+    console.log("Building the project ...");
+});
